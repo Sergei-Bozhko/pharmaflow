eeab34b7ca9c4ee08f46236438a13c1d481d818c
Sergei Bozhko <serg.bozhko@outlook.com>
2026-05-10 14:36:00 +0600

    feat(infra): added Audit event migration

---
+++ src/PharmaFlow.Infrastructure/Persistence/Migrations/20260510073345_AuditEventIdentityKey.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace PharmaFlow.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AuditEventIdentityKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "id",
+                table: "audit_events",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "id",
+                table: "audit_events",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+        }
+    }
+}
