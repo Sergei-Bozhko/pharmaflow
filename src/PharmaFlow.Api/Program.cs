025c844fc986688a9d46ba1b8824c14160f6973b
Sergei Bozhko <serg.bozhko@outlook.com>
2026-05-30 21:27:04 +0600

    feat(api): add openApi doc + scalar ui

--- src/PharmaFlow.Api/Program.cs
+++ src/PharmaFlow.Api/Program.cs
@@ -4,19 +4,23 @@ using PharmaFlow.Application;
 using PharmaFlow.Application.Common.Idempotency;
 using PharmaFlow.Infrastructure;
 
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.WebHost.ConfigureKestrel(options => options.AddServerHeader = false);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi("v1");
 builder.Services.AddPharmaFlowApplication();
 builder.Services.AddPharmaFlowInfrastructure(builder.Configuration);
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddScoped<IIdempotencyKeyProvider, HttpIdempotencyKeyProvider>();
 
 var app = builder.Build();
+
+app.MapScalarApiReference();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
