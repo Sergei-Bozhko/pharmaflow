f9b6979ec5bcb3b5de0705767965933a531a9c30
Sergei Bozhko <serg.bozhko@outlook.com>
2026-05-31 16:11:39 +0600

    feat(api): added get by id to endpoints

--- src/PharmaFlow.Api/Endpoints/Studies/StudiesEndpoints.cs
+++ src/PharmaFlow.Api/Endpoints/Studies/StudiesEndpoints.cs
@@ -2,6 +2,8 @@ using Mediator;
 
 using PharmaFlow.Api.Common;
 using PharmaFlow.Application.Studies.Commands.CreateStudy;
+using PharmaFlow.Application.Studies.Queries.GetStudyById;
+using PharmaFlow.Domain.Common.Ids;
 
 namespace PharmaFlow.Api.Endpoints.Studies;
 
@@ -29,6 +31,14 @@ public static class StudiesEndpoints
             var result = await sender.Send(cmd, ct);
 
             return result.ToCreatedResult(ctx, id => $"{ctx.Request.Path}/{id.Value}");
+        });
+
+        group.MapGet("{id:guid}", async (Guid id, ISender sender, HttpContext ctx, CancellationToken ct) =>
+        {
+            var query = new GetStudyByIdQuery(new StudyId(id));
+            var result = await sender.Send(query, ct);
+
+            return result.ToHttpResult(ctx);
         });
 
         return app;
