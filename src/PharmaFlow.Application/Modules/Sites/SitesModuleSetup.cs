9b590bafaf6e112b2d189d38a6a1b4a75ae4e6f8
Sergei Bozhko <serg.bozhko@outlook.com>
2026-06-01 16:06:46 +0600

    feat(app): add sites module setup class

---
+++ src/PharmaFlow.Application/Modules/Sites/SitesModuleSetup.cs
@@ -0,0 +1,8 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PharmaFlow.Application.Modules.Sites;
+
+public static class SitesModuleSetup
+{
+    public static IServiceCollection AddSitesModule(this IServiceCollection services) => services;
+}
